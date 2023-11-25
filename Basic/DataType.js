3c0f71969fdb1343181cb85cd882e315a401b58c
ankitpaal542 <ankitpaal542@gmail.com>
2023-11-26 00:03:19 +0530

    DataType

---
+++ Basic/DataType.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3); // we are using nodejs,  not browser
+
+console.log(3
+  +   // code readability
+  3);
+console.log("hitesh");
+
+let name ="Himanshu";
+let age = 18;
+let isLogin = true;
+
+
+// number => 2 2 to power 53
+// bigInt
+//Boolean =>  true / false
+//null  => Standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object 
+console.log(typeof "Ankit");
+console.log(typeof 33);
+console.log(typeof '33');
+
+
--- Basic/variable.js
+++ Basic/variable.js
@@ -1,9 +1,17 @@
+// can'nt change
 const accountId = 12234;
+
 let name = "Ankit pal";
 var email = 'ankitpaal542@gmail.com';
+accountCity = "Jaipur";// not a good practice 
+let accountState;
+//accountId = 9;// not allowed 
+//console.log(accountId);
 console.log(name);
 console.log("Ankit");
-console.table([accountId, name, email])
+console.table([accountId, name, email, accountCity,accountState]);
 
 // variable 
- 
+ /*
+ prefer not to use var becouse of issue in block scope and functional scope
+ */
