e375dd076194addca58a4a2552c669cf76516fcb
ankitpaal542 <ankitpaal542@gmail.com>
2023-11-25 23:11:39 +0530

    Learn java Script

---
+++ Basic/variable.js
@@ -0,0 +1,9 @@
+const accountId = 12234;
+let name = "Ankit pal";
+var email = 'ankitpaal542@gmail.com';
+console.log(name);
+console.log("Ankit");
+console.table([accountId, name, email])
+
+// variable 
+ 
